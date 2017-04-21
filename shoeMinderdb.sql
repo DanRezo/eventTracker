2f0dca3272629b32c9b981a8945bca8981e4e1cf
DanRezo <DrEzO@DaNReZO.local>
2017-04-21 17:34:17 -0600

    changed project to shoeMinder

---
+++ shoeMinderdb.sql
@@ -0,0 +1,36 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `mydb` ;
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
+USE `mydb` ;
+
+-- -----------------------------------------------------
+-- Table `Shoe`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `Shoe` ;
+
+CREATE TABLE IF NOT EXISTS `Shoe` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `currentMileage` INT NULL,
+  `totalMilage` INT NULL,
+  `brand` VARCHAR(45) NULL,
+  `name` VARCHAR(45) NULL,
+  `image` VARCHAR(255) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
