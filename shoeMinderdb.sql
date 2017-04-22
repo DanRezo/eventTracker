06c2338058631c0167d8df3169972cf52da99c73
DanRezo <DrEzO@DaNReZO.local>
2017-04-21 18:08:04 -0600

    added db

--- shoeMinderdb.sql
+++ shoeMinderdb.sql
@@ -5,15 +5,15 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema shoeMinder
 -- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `mydb` ;
+DROP SCHEMA IF EXISTS `shoeMinder` ;
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema shoeMinder
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
+CREATE SCHEMA IF NOT EXISTS `shoeMinder` DEFAULT CHARACTER SET utf8 ;
+USE `shoeMinder` ;
 
 -- -----------------------------------------------------
 -- Table `Shoe`
@@ -34,3 +34,20 @@ ENGINE = InnoDB;
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+-- -----------------------------------------------------
+-- Data for table `Shoe`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `shoeMinder`;
+INSERT INTO `Shoe` (`id`, `currentMileage`, `totalMilage`, `brand`, `name`, `image`) VALUES (1, NULL, 23, 'Nike', 'Vomero 13', NULL);
+INSERT INTO `Shoe` (`id`, `currentMileage`, `totalMilage`, `brand`, `name`, `image`) VALUES (2, NULL, 56, 'Nike', 'Pegasus 34', NULL);
+INSERT INTO `Shoe` (`id`, `currentMileage`, `totalMilage`, `brand`, `name`, `image`) VALUES (3, NULL, 77, 'New Balance', '1080 V6', NULL);
+INSERT INTO `Shoe` (`id`, `currentMileage`, `totalMilage`, `brand`, `name`, `image`) VALUES (4, NULL, 235, 'Altra', 'Torin 3.5', NULL);
+INSERT INTO `Shoe` (`id`, `currentMileage`, `totalMilage`, `brand`, `name`, `image`) VALUES (5, NULL, 300, 'Newton', 'Kismet 3', NULL);
+INSERT INTO `Shoe` (`id`, `currentMileage`, `totalMilage`, `brand`, `name`, `image`) VALUES (6, NULL, 345, 'New Balance', '860V6', NULL);
+INSERT INTO `Shoe` (`id`, `currentMileage`, `totalMilage`, `brand`, `name`, `image`) VALUES (7, NULL, 12, 'Brooks', 'Ghost 10', NULL);
+INSERT INTO `Shoe` (`id`, `currentMileage`, `totalMilage`, `brand`, `name`, `image`) VALUES (8, NULL, 3, 'Asics', 'Nimbus 11', NULL);
+
+COMMIT;
+
